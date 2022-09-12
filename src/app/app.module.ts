786ac1bdbe4fe7d2d6e6d75e8ad2350829752ee8
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-09-12 18:03:15 +0100

    Piece initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChordsComponent } from './chords/chords.component';
 import { ScalesComponent } from './scales/scales.component';
 import { ProgressionsComponent } from './progressions/progressions.component';
+import { PiecesComponent } from './pieces/pieces.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { ProgressionsComponent } from './progressions/progressions.component';
     ChordsComponent,
     ScalesComponent,
     ProgressionsComponent,
+    PiecesComponent,
   ],
   imports: [
     BrowserModule,
