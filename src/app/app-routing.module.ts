42ad7ddb9f0a46f03bbef0a4558ed7d54d459f9e
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-11-02 05:15:01 +0000

    Remove all the half finished stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ChordsComponent } from './chords/chords.component';
 import { ScalesComponent } from './scales/scales.component';
-import { ProgressionsComponent } from './progressions/progressions.component';
-import { PiecesComponent } from './pieces/pieces.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/chords', pathMatch: 'full' },
   { path: 'chords', component: ChordsComponent },
   { path: 'scales', component: ScalesComponent },
-  { path: 'progressions', component: ProgressionsComponent },
-  { path: 'pieces', component: PiecesComponent },
 ];
 
 @NgModule({
