2fe66ac78240fbfdfd5b86d7914e30fb0d8a886a
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-11-02 05:13:49 +0000

    Add pieces

--- src/app/pieces/pieces.ts
+++ src/app/pieces/pieces.ts
@@ -1,27 +1,47 @@
 import { numToString } from '../data';
 import { chordpatterns } from '../data';
 
+const FQBK_handbook_1 = [
+  'Bb 7',
+  'Eb 7',
+  'Bb 7',
+  'F m7',
+  'Bb 7',
+  //
+  'Eb 7',
+  'Eb 7',
+  'Bb 7',
+  'D dim',
+  'G 7',
+  //
+  'C m7',
+  'F 7',
+  'D m7',
+  'G 7',
+  'C m7',
+  'F 7',
+];
+
+const allTheThingsYouAre = [
+  'F m7',
+  'Bb m7',
+  'Eb 7',
+  'Ab maj7',
+  //
+  'Db maj7',
+  'G 7',
+  'C maj7',
+  'C maj7',
+  //
+  'C m7',
+  'F m7',
+  'Bb 7',
+  'Eb maj7',
+  //
+];
+
 export class PieceQuestionGenerator {
-  public chords = [
-    'Bb 7',
-    'Eb 7',
-    'Bb 7',
-    'F m7',
-    'Bb 7',
-    //
-    'Eb 7',
-    'Eb 7',
-    'Bb 7',
-    'D dim',
-    'G 7',
-    //
-    'C m7',
-    'F 7',
-    'D m7',
-    'G 7',
-    'C m7',
-    'F 7',
-  ];
+  public chords = allTheThingsYouAre;
   private currentQuestionIndex = 0;
   public question = '';
 
